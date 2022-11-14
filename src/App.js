d85166f24ad464afb56b6b349245b90d6f9675c6
adhxabre <abeldustin06@gmail.com>
2022-11-14 14:27:23 +0700

    useNavigate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import Home from './pages/Home'
 import About from './pages/About'
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,17 @@
+import { useNavigate, Link } from 'react-router-dom'
+
 export default function Home() {
-    return <h1>Home</h1>
+    const navigate = useNavigate()
+
+    const handleNavigateToSignin = () => {
+        navigate("/signin")
+    }
+
+    return(
+        <>
+            <h1>Home</h1>
+            <button onClick={handleNavigateToSignin}>Click to Sign In</button>
+            <Link to="/profile">Profile</Link>
+        </>
+    )
 }
